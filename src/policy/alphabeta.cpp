869f88f663121719c9a397a47a8d453c8c94da97
NonG <111571440+FarmerLiong@users.noreply.github.com>
2023-06-18 21:31:22 +0800

    2023/06/18 bad Alphabeta

--- src/policy/alphabeta.cpp
+++ src/policy/alphabeta.cpp
@@ -1,17 +1,17 @@
 #include <cstdlib>
 
 #include "../state/state.hpp"
-#include "./minimax.hpp"
+#include "./alphabeta.hpp"
 
 
 /**
- * @brief get a legal action by minimax
+ * @brief get a legal action by alphabeta
  * 
  * @param state Now state
  * @param depth 
  * @return Move 
  */
-Move Minimax::get_move(State *state, int depth, int player){
+Move Alphabeta::get_move(State *state, int depth, int player){
   if(!state->legal_actions.size())
     state->get_legal_actions();
   auto actions = state->legal_actions;
@@ -28,23 +28,26 @@ Move Minimax::get_move(State *state, int depth, int player){
   return retmove;
 }
 
-int Minimax::get_value(State *state, int depth, int player){
+int Alphabeta::get_value(State *state, int depth, int player, int alpha, int beta){
   int value; 
   if(depth == 0)
     return state->player == player ? state->evaluate() : -1*state->evaluate();
   if(state->player == player){
     value = -1e9;
     for(auto& move:state->legal_actions){
-      value = std::max(value, get_value(state->next_state(move), depth-1, player));
+      value = std::max(value, get_value(state->next_state(move), depth-1, player, alpha, beta));
+      alpha = std::max(alpha, value);
+      if(alpha >= beta) break;
     }
     return value;
   }
   else{
     value = 1e9;
     for(auto& move:state->legal_actions){
-      value = std::min(value, get_value(state->next_state(move), depth-1, player));
+      value = std::min(value, get_value(state->next_state(move), depth-1, player, alpha, beta));
+      beta = std::min(beta, value);
+      if(beta <= alpha) break;
     }
     return value;
   }
-
 }
