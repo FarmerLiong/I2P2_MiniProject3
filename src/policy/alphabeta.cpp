1587f19326e427c35e4fa0a4fc57d15a000bb5dd
NonG <111571440+FarmerLiong@users.noreply.github.com>
2023-06-19 14:42:13 +0800

    2023/06/18 Debug & refactor

--- src/policy/alphabeta.cpp
+++ src/policy/alphabeta.cpp
@@ -15,11 +15,11 @@ Move Alphabeta::get_move(State *state, int depth, int player){
   if(!state->legal_actions.size())
     state->get_legal_actions();
   auto actions = state->legal_actions;
-  int max_value = -1e9, state_value;
+  int max_value = -1e9;
   Move retmove = actions[0];
 
   for(auto& move:actions){
-    state_value = get_value(state->next_state(move), depth-1, player);
+    int state_value = get_value(state->next_state(move), depth-1, player);
     if(state_value > max_value){
       max_value = state_value;
       retmove = move;
@@ -38,10 +38,11 @@ int Alphabeta::get_value(State *state, int depth, int player, int alpha, int bet
   if(depth == 0)
     return state->player == player ? state->evaluate() : -1*state->evaluate();
 
+  auto actions = state->legal_actions;
   if(state->player == player){
     value = -1e9;
 
-    for(auto& move:state->legal_actions){
+    for(const auto& move:actions){
       value = std::max(value, get_value(state->next_state(move), depth-1, player, alpha, beta));
       alpha = std::max(alpha, value);
       if(alpha >= beta) break;
@@ -51,7 +52,7 @@ int Alphabeta::get_value(State *state, int depth, int player, int alpha, int bet
   else{
     value = 1e9;
 
-    for(auto& move:state->legal_actions){
+    for(const auto& move:actions){
       value = std::min(value, get_value(state->next_state(move), depth-1, player, alpha, beta));
       beta = std::min(beta, value);
       if(beta <= alpha) break;
--- src/policy/minimax.cpp
+++ src/policy/minimax.cpp
@@ -15,11 +15,11 @@ Move Minimax::get_move(State *state, int depth, int player){
   if(!state->legal_actions.size())
     state->get_legal_actions();
   auto actions = state->legal_actions;
-  int max_value = -1e9, state_value;
+  int max_value = -1e9;
   Move retmove = actions[0];
 
   for(auto& move:actions){
-    state_value = get_value(state->next_state(move), depth-1, player);
+    int state_value = get_value(state->next_state(move), depth-1, player);
     if(state_value > max_value){
       max_value = state_value;
       retmove = move;
@@ -32,16 +32,20 @@ int Minimax::get_value(State *state, int depth, int player){
   int value; 
   if(depth == 0)
     return state->player == player ? state->evaluate() : -1*state->evaluate();
+  
+  auto actions = state->legal_actions;
   if(state->player == player){
     value = -1e9;
-    for(auto& move:state->legal_actions){
+
+    for(auto& move:actions){
       value = std::max(value, get_value(state->next_state(move), depth-1, player));
     }
     return value;
   }
   else{
     value = 1e9;
-    for(auto& move:state->legal_actions){
+    
+    for(auto& move:actions){
       value = std::min(value, get_value(state->next_state(move), depth-1, player));
     }
     return value;
